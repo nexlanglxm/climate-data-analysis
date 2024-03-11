9b257159168a810ab64005fc78f351959df60102
Neil Anglim <neiljanglim@gmail.com>
2024-03-11 18:48:03 +0000

    imprving version control hygiene

--- plotting/w2.1.2dplot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-'''
-the default system is not necessarily accurate for squares
-but could it be programmed to be more so...using a larger set of data? definitely
-'''
-plt.plot([2, 3, 5, 7], [4, 9, 25, 49])
-plt.ylabel('random numbers')
-plt.show()
